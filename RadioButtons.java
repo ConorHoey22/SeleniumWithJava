4a0cca1c24e89214bd5cdf404be2fb510c308314
ConorHoey22 <conorhoey133@gmail.com>
2023-05-30 23:28:23 +0100

    Radio button

---
+++ RadioButtons.java
@@ -0,0 +1,28 @@
+package TestPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RadioButtons {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		// TODO Auto-generated method stub
+		//Key and location of ChromeDriver
+		System.setProperty("webdriver.chrome.driver" , "C:\\Users\\Conor\\OneDrive\\Documents\\FD\\Portfolio\\Web Automation\\drivers\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.automationtesting.co.uk/dropdown.html");
+
+		// Locate RadioButton 
+		
+		driver.findElement(By.cssSelector("[for='demo-priority-normal']")).click();
+		
+	
+	}
+
+}
